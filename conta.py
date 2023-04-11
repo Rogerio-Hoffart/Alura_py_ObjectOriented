5853e2cbdd6b5ddc627daab1857b3de5a76f785a
Rogerio-Hoffart <rjhmribeiro@outlook.com>
2023-04-11 15:20:25 -0300

    update

--- conta.py
+++ conta.py
@@ -8,3 +8,12 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposito(self, valor):
+        self.saldo += self.valor
+
+    def saque(self, valor):
+        self.saldo -= valor
