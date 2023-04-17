a134535442d95b929212d087c8aedde62e8f4b21
Rogerio-Hoffart <rjhmribeiro@outlook.com>
2023-04-17 15:08:53 -0300

    update

--- teste.py
+++ teste.py
@@ -1,18 +1,36 @@
-
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.nome = nome
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
+        self._likes = 0
+    @property
+    def likes(self):
+        return self._likes
+    def dar_like(self):
+        self._likes += 1
+    @property
+    def nome(self):
+        return self._nome
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
         self.duracao = duracao
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
 
+
+
 vingadores = Filme('vingadores - gerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+vingadores.dar_like()
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - LIKES: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - LIKES: {atlanta.likes}')
