924ca37f9d6c7e453a6f6390d42e38af6dca96a0
Rogerio-Hoffart <rjhmribeiro@outlook.com>
2023-04-19 14:36:57 -0300

    update

--- praticando.py
+++ praticando.py
@@ -1,4 +1,3 @@
-
 class Animal:
     def __init__(self, name, color):
         self.name = name
@@ -7,18 +6,23 @@ class Animal:
     def make_sound(self):
         pass
 
+
 class Dog(Animal):
     def make_sound(self):
         return "Au Au Au!"
+
 
 class Cat(Animal):
     def make_sound(self):
         return "Miau!"
 
-animals = [Dog("Tigor", "preto"), Dog("LILIca", "preta"), Dog("Belinha", "Branca"), Cat("Teo", "cinza"), Cat("Astolfo", "Marrom Claro")]
+
+animals = [Dog("Tigor", "preto"), Dog("LILIca", "preta"), Dog("Belinha", "Branca"), Cat("Teo", "cinza"),
+           Cat("Astolfo", "Marrom Claro")]
 
 for animal in animals:
-    print(animal.name +' '+ animal.color + ": " + animal.make_sound())
+    print(animal.name + ' ' + animal.color + ": " + animal.make_sound())
+
 
 class Shape:
     def __init__(self, color):
@@ -26,6 +30,7 @@ class Shape:
 
     def area(self):
         pass
+
 
 class Rectangle(Shape):
     def __init__(self, color, width, height):
@@ -36,15 +41,27 @@ class Rectangle(Shape):
     def area(self):
         return self.width * self.height
 
+
 class Circle(Shape):
     def __init__(self, color, radius):
         super().__init__(color)
         self.radius = radius
 
     def area(self):
-        return 3.14 * self.radius ** 2
+        return 3.1415 * self.radius ** 2
 
-shapes = [Rectangle("blue", 5, 10), Circle("red", 7)]
+
+class Triangle(Shape):
+    def __init__(self, color, width, height):
+        super().__init__(color)
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return (self.width * self.height) / 2
+
+
+shapes = [Rectangle("blue", 5, 10), Circle("red", 7), Triangle("yelow", 5, 10)]
 
 for shape in shapes:
     print(shape.color + " " + str(shape.area()))
