64c0d0928d4f2e937688095983c23fde58c3bd2c
Rogerio-Hoffart <rjhmribeiro@outlook.com>
2023-04-19 10:33:03 -0300

    update

--- praticando.py
+++ praticando.py
@@ -19,3 +19,32 @@ animals = [Dog("Tigor", "preto"), Dog("LILIca", "preta"), Dog("Belinha", "Branca
 
 for animal in animals:
     print(animal.name +' '+ animal.color + ": " + animal.make_sound())
+
+class Shape:
+    def __init__(self, color):
+        self.color = color
+
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, color, width, height):
+        super().__init__(color)
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, color, radius):
+        super().__init__(color)
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+shapes = [Rectangle("blue", 5, 10), Circle("red", 7)]
+
+for shape in shapes:
+    print(shape.color + " " + str(shape.area()))
